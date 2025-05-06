15126d6827e12a0a7fb787377d1acc1001ddf2fa
Einar Hansen <49709354+einar-hansen@users.noreply.github.com>
2025-05-06 15:27:13 +0200

    Add environment variables

--- config/toolkit.php
+++ config/toolkit.php
@@ -10,18 +10,18 @@ return [
     |
     */
     'eloquent' => [
-        'strict_mode' => true,
-        'eager_load_relationships' => false,
+        'strict_mode' => env('ELOQUENT_STRICT_MODE', true),
+        'eager_load_relationships' => env('ELOQUENT_EAGER_LOAD_RELATIONSHIPS', false),
     ],
     'app' => [
-        'enable_aggressive_prefetching' => true,
-        'enforce_https_scheme' => true,
-        'enable_immutable_dates' => true,
-        'disable_destructive_commands' => true,
-        'use_default_password' => true,
+        'enable_aggressive_prefetching' => env('APP_ENABLE_AGGRESSIVE_PREFETCHING', true),
+        'enforce_https_scheme' => env('APP_ENFORCE_HTTPS_SCHEME', true),
+        'enable_immutable_dates' => env('APP_ENABLE_IMMUTABLE_DATES', true),
+        'disable_destructive_commands' => env('APP_DISABLE_DESTRUCTIVE_COMMANDS', true),
+        'use_default_password' => env('APP_USE_DEFAULT_PASSWORD', true),
     ],
     'tests' => [
-        'enable_fake_sleep' => true,
-        'prevent_stray_requests' => true,
+        'enable_fake_sleep' => env('TESTS_ENABLE_FAKE_SLEEP', true),
+        'prevent_stray_requests' => env('TESTS_PREVENT_STRAY_REQUESTS', true),
     ],
 ];
